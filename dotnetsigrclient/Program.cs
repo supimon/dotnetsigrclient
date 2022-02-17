76672ba5d0316fe04badfd4b8ad24c0e59334f2e
supimon <supimon@gmail.com>
2022-02-17 20:01:51 +0530

    Add project files.

---
+++ dotnetsigrclient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace dotnetsigrclient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Preparing the connection..");
+            
+            // setting up the proxy hub
+            HubConnection connection = new HubConnectionBuilder()
+                .WithUrl("https://api.skchase.com/checkout")
+                .Build();
+
+            // start connection
+            await connection.StartAsync();
+            Console.WriteLine("Connected");
+
+            // get vouchers from skchase for a sample SalesChannelID
+            Object str = await connection.InvokeAsync<Object>("GetVouchers", "680cbfbc-76a6-cb30-4c04-39b26251f870");
+            
+            // print vouchers to console
+            Console.WriteLine("Check this out : " + str);
+
+            // keep the console alive to view result
+            Console.ReadKey();
+        }
+
+    }
+}
